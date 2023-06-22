55bf89f79ed58e0e8891f86defc7812ad9f8a4a3
adrgs <albastroiudragos@gmail.com>
2023-06-22 16:06:58 -0400

    Refactor poison_model.ipynb

--- get_dataset.py
+++ get_dataset.py
@@ -5,20 +5,12 @@ Each image in the dataset is a numpy array of shape (32, 32, 3), with the values
 being unsigned integers (i.e., in the range 0,1,...,255).
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import cv2
 import random
 import pickle
 import os
 import sys
 import torch
-
-# import tensorflow as tf
-# import tensorflow.compat.v1 as tf
-# tf.disable_v2_behavior()
 
 version = sys.version_info
 
@@ -31,12 +23,63 @@ from torchvision import transforms
 from tensors_dataset import TensorDataset
 from multiprocessing.dummy import Pool as ThreadPool
 from utils import *
+import torchvision
 
 configs = read_config()
-dataset = configs["dataset"]
 
 
-def get_dataset(filedir, max_num=0):
+def load_training(compressed, train_dataset_name, com_ratio):
+    if compressed:
+        train_dataset = torch.load(
+            "./dataset/compression_" + train_dataset_name + "_" + str(com_ratio)
+        )
+    else:
+        train_dataset = torch.load("./dataset/distill_" + train_dataset_name)
+
+    print("distill_data num:", len(train_dataset))
+    train_images = []
+    train_labels = []
+    for i in range(len(train_dataset)):
+        img = train_dataset[i][0]
+        label = train_dataset[i][1].cpu()
+        train_images.append(img)
+        train_labels.append(label)
+    train_images = np.array(train_images)
+    train_labels = np.array(train_labels)
+
+    print("load train data finished")
+
+    print(type(train_images), type(train_images[0]))
+    print(type(train_labels), type(train_labels[0]))
+
+    return train_images, train_labels
+
+
+def load_testing(test_dataset_name):
+    datasets = ["cifar10", "cifar100", "lfw", "gtsrb", "celeba"]
+    test_dataset_name = test_dataset_name.lower()
+
+    if test_dataset_name not in datasets:
+        raise ValueError("Dataset not supported, please choose from " + str(datasets))
+    
+    if test_dataset_name == "cifar10":
+        dataset = torchvision.datasets.CIFAR10(root="./data", train=False, download=True)
+    elif test_dataset_name == "cifar100":
+        dataset = torchvision.datasets.CIFAR100(root="./data", train=False, download=True)
+    elif test_dataset_name == "lfw":
+        dataset = torchvision.datasets.LFWPeople(root="./data", train=False, download=True)
+    elif test_dataset_name == "gtsrb":
+        dataset = torchvision.datasets.GTSRB(root="./data", train=False, download=True)
+    elif test_dataset_name == "celeba":
+        dataset = torchvision.datasets.CelebA(root="./data", train=False, download=True)
+
+    test_images = [dataset[i][0] for i in range(len(dataset))]
+    test_labels = dataset.targets
+
+    return test_images, test_labels
+
+
+def get_dataset(filedir, dataset="TODO: refactor", max_num=0):
     label_num = len(os.listdir(filedir))
 
     namelist = []
@@ -63,14 +106,4 @@ def get_dataset(filedir, max_num=0):
                 if n == max_num:
                     break
 
-    # pool = ThreadPool()
-    # pool.map(read_images, list(range(label_num)))
-    # pool.close()
-    # pool.join()
-    #
-    # Together = list(zip(images, labels))
-    # random.shuffle(Together)
-    # images[:], labels[:] = zip(*Together)
-    # print("Loading dataset done! Load " + str(len(labels)) + " images in total.")
-    # return images, labels
     return load_dataset_shuffled(read_images, label_num)
